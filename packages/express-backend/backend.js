d1963225bc195bdee3007bc254fb334cd8eeb85b
Isabelle Ford <isabelleford@Mac.lan>
2026-01-20 12:06:05 -0800

    Add GET /users/:id endpoint

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -35,17 +35,22 @@ const users = {
 const app = express();
 const port = 8000;
 
-app.use(express.json());
-
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
+const findUserById = (id) =>
+  users["users_list"].find((user) => user["id"] === id);
 
 const findUserByName = (name) => {
   return users["users_list"].filter(
     (user) => user["name"] === name
   );
 };
+
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
 
 app.get("/users", (req, res) => {
   const name = req.query.name;
@@ -56,6 +61,18 @@ app.get("/users", (req, res) => {
     res.send(result);
   } else {
     res.send(users);
+  }
+});
+
+app.get("/users/:id", (req, res) => {
+  const id = req.params.id;
+
+  let result = findUserById(id);
+
+  if (result === undefined) {
+    res.status(404).send("Resource not found.");
+  } else {
+    res.send(result);
   }
 });
 
